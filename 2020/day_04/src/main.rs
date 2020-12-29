33bed143f48d2612fdf37792de05eb7f15ab7aa3
Fabien Bellanger <fabien.bellanger@apitic.com>
2020-12-30 00:37:49 +0100

    Day 4 Part 1 finished

---
+++ 2020/day_04/src/main.rs
@@ -0,0 +1,101 @@
+use std::fs;
+
+#[derive(Debug)]
+struct Passport {
+    ecl: String,
+    pid: String,
+    eyr: usize,
+    hcl: String,
+    byr: usize,
+    iyr: usize,
+    cid: Option<usize>,
+    hgt: String,
+}
+
+impl Passport {
+    fn new() -> Self {
+        Self {
+            ecl: "".to_owned(),
+            pid: "".to_owned(),
+            eyr: 0,
+            hcl: "".to_owned(),
+            byr: 0,
+            iyr: 0,
+            cid: None,
+            hgt: "".to_owned(),
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        self.ecl != "".to_owned()
+            && self.pid != "".to_owned()
+            && self.eyr != 0
+            && self.hcl != "".to_owned()
+            && self.byr != 0
+            && self.iyr != 0
+            && self.hgt != "".to_owned()
+    }
+}
+
+fn main() {
+    println!("Part 1 result: {}", part1(get_data()));
+}
+
+fn part1(passports: Vec<Passport>) -> usize {
+    let mut valid = 0;
+    for passport in passports.iter() {
+        if passport.is_valid() {
+            valid += 1;
+        }
+    }
+
+    valid
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(2, part1(_get_data_test()));
+    assert_eq!(239, part1(get_data()));
+}
+
+fn get_passport(data: String) -> Vec<Passport> {
+    let lines: Vec<&str> = data.split("\n\n").collect();
+
+    let mut passports: Vec<Passport> = Vec::new();
+    for line in lines.iter() {
+        let line = line.replace("\n", " ");
+        let parts = line.split_whitespace();
+        let mut passport = Passport::new();
+
+        for part in parts {
+            let mut field = part.split(":");
+            let field_name = field.next().unwrap();
+            let field_value = field.next().unwrap();
+            match field_name {
+                "ecl" => passport.ecl = field_value.to_owned(),
+                "pid" => passport.pid = field_value.to_owned(),
+                "eyr" => passport.eyr = field_value.parse().expect("invalid eyr"),
+                "hcl" => passport.hcl = field_value.to_owned(),
+                "byr" => passport.byr = field_value.parse().expect("invalid byr"),
+                "iyr" => passport.iyr = field_value.parse().expect("invalid iyr"),
+                "cid" => passport.cid = Some(field_value.parse().expect("invalid cid")),
+                "hgt" => passport.hgt = field_value.to_owned(),
+                _ => (),
+            };
+        }
+
+        passports.push(passport);
+    }
+
+    passports
+}
+
+fn get_data() -> Vec<Passport> {
+    let data = fs::read_to_string("input.txt").expect("Cannot read the file input.txt");
+    get_passport(data)
+}
+
+fn _get_data_test() -> Vec<Passport> {
+    let data = fs::read_to_string("test.txt").expect("Cannot read the file test.txt");
+    get_passport(data)
+}
--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 
 -  [x] Day 01 : Rust
 -  [x] Day 02 : Go
--  [ ] Day 03 : Rust
--  [ ] Day 04 : 
+-  [x] Day 03 : Rust
+-  [ ] Day 04 : Rust
 -  [ ] Day 05 : 
 -  [ ] Day 06 : 
 -  [ ] Day 07 : 
