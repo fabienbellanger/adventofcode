e7cf950c79cdc322304c482060c59585a7de4a6b
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-01-06 23:20:55 +0100

    Day 9 finished

--- 2020/day_09/src/main.rs
+++ 2020/day_09/src/main.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
 fn main() {
-    println!("Part 1 result: {}", part1(get_data(), 25).unwrap());
-    println!("Part 2 result: {}", part2(get_data(), 25).unwrap());
+    println!("Part 1 result: {}", part1(&get_data(), 25).unwrap());
+    println!("Part 2 result: {}", part2(&get_data(), 25).unwrap());
 }
 
-fn part1(data: Vec<usize>, preamble: usize) -> Option<usize> {
+fn part1(data: &[usize], preamble: usize) -> Option<usize> {
     for i in preamble..data.len() {
         let mut ok = false;
-        'sum: for j in i-preamble..i {
-            for k in j+1..i {
+        'sum: for j in i - preamble..i {
+            for k in j + 1..i {
                 let sum = data[j] + data[k];
                 if sum == data[i] {
                     ok = true;
@@ -26,9 +26,30 @@ fn part1(data: Vec<usize>, preamble: usize) -> Option<usize> {
     None
 }
 
-fn part2(data: Vec<usize>, preamble: usize) -> Option<usize> {
-    let invalid_number = part1(data, preamble).unwrap();
-    dbg!(invalid_number);
+fn part2(data: &[usize], preamble: usize) -> Option<usize> {
+    get_encryption_weakness(data, part1(data, preamble).unwrap())
+}
+
+fn get_encryption_weakness(data: &[usize], invalid_number: usize) -> Option<usize> {
+    let mut values: Vec<usize> = Vec::new();
+
+    for (i, current) in data.iter().enumerate() {
+        values.push(*current);
+
+        for next in data.iter().skip(i + 1) {
+            values.push(*next);
+
+            let sum: usize = values.iter().sum();
+            if sum == invalid_number {
+                values.sort_unstable();
+                return Some(values[0] + values[values.len() - 1]);
+            } else if sum > invalid_number {
+                values.clear();
+                break;
+            }
+        }
+    }
+
     None
 }
 
@@ -52,12 +73,12 @@ fn _get_data_test() -> Vec<usize> {
 
 #[test]
 fn test_part1() {
-    assert_eq!(Some(127), part1(_get_data_test(), 5));
-    assert_eq!(Some(530627549), part1(get_data(), 25));
+    assert_eq!(Some(127), part1(&_get_data_test(), 5));
+    assert_eq!(Some(530627549), part1(&get_data(), 25));
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(Some(62), part2(_get_data_test(), 5));
-    // assert_eq!(Some(530627549), part1(get_data(), 25));
+    assert_eq!(Some(62), part2(&_get_data_test(), 5));
+    assert_eq!(Some(77730285), part2(&get_data(), 25));
 }
