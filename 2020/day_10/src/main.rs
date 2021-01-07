461d493314e039860ccf27dc27673327810d8e43
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-01-07 22:00:50 +0100

    Day 10 part 1 finished

--- 2020/day_10/src/main.rs
+++ 2020/day_10/src/main.rs
@@ -1,17 +1,39 @@
 use std::fs;
 
 fn main() {
-    println!("Part 1 result: {}", part1(&get_data()).unwrap());
-    println!("Part 2 result: {}", part2(&get_data()).unwrap());
+    println!("Part 1 result: {}", part1(&mut get_data()));
+    println!("Part 2 result: {}", part2(&get_data()));
 }
 
-fn part1(data: &[usize]) -> Option<usize> {
-    dbg!(data);
-    None
+fn part1(data: &mut [usize]) -> usize {
+    data.sort_unstable();
+
+    let mut diff_1 = 0;
+    let mut diff_3 = 1;
+
+    for (index, current) in data.iter().take(data.len() - 1).enumerate() {
+        // Start with 0 value
+        if index == 0 {
+            match current {
+                1 => diff_1 += 1,
+                3 => diff_3 += 1,
+                _ => (),
+            }
+        }
+
+        let diff = data[index + 1] - current;
+        match diff {
+            1 => diff_1 += 1,
+            3 => diff_3 += 1,
+            _ => (),
+        }
+    }
+
+    diff_1 * diff_3
 }
 
-fn part2(data: &[usize]) -> Option<usize> {
-    None
+fn part2(data: &[usize]) -> usize {
+    0
 }
 
 fn get_data() -> Vec<usize> {
@@ -34,12 +56,12 @@ fn _get_data_test() -> Vec<usize> {
 
 #[test]
 fn test_part1() {
-    assert_eq!(Some(22), part1(&_get_data_test()));
+    assert_eq!(220, part1(&mut _get_data_test()));
     // assert_eq!(Some(), part1(&get_data()));
 }
 
 #[test]
 fn test_part2() {
-    // assert_eq!(Some(), part2(&_get_data_test()));
-    // assert_eq!(Some(), part2(&get_data()));
+    // assert_eq!(, part2(&mut _get_data_test()));
+    // assert_eq!(, part2(&mut get_data()));
 }
