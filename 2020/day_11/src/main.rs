23a0848161f04a152f1b7f361e9579f74bb9d80c
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-01-11 23:50:19 +0100

    Maigre avancement

--- 2020/day_11/src/main.rs
+++ 2020/day_11/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum SeatState {
     Floor,
     Empty,
@@ -13,20 +13,125 @@ fn main() {
 }
 
 fn part1(seats: Vec<Vec<SeatState>>) -> usize {
-    dbg!(&seats);
+    let mut last: Vec<Vec<SeatState>> = seats.clone();
+    let mut new_seats: Vec<Vec<SeatState>> = Vec::new();
+    loop {
+        new_seats.clear();
+        for (row_index, row) in seats.iter().enumerate() {
+            new_seats.push(Vec::new());
+            for (col_index, _col) in row.iter().enumerate() {
+                new_seats[row_index].push(apply_rules(&seats, row_index, col_index));
+            }
+        }
 
-    for (row_index, row) in seats.iter().enumerate() {
-        for (col_index, col) in row.iter().enumerate() {
-            apply_rules(&seats, row_index, col_index);
+        if is_seats_equal(&last, &new_seats) {
+            break;
+        } else {
+            last = new_seats.clone();
         }
     }
+
+    dbg!(&last);
 
     0
 }
 
+fn is_seats_equal(seat_1: &Vec<Vec<SeatState>>, seat_2: &Vec<Vec<SeatState>>) -> bool {
+    true
+}
+
 fn apply_rules(seats: &Vec<Vec<SeatState>>, row_index: usize, col_index: usize) -> SeatState {
-    println!("row_index={}, col_index={}", row_index, col_index);
-    SeatState::Empty
+    match seats[row_index][col_index] {
+        SeatState::Floor => SeatState::Floor,
+        SeatState::Empty => {
+            // If no seat occupied => occupied
+            let mut number_of_occuped = 0;
+            let mut row_min: isize = -1;
+            let mut row_max: isize = 1;
+            let mut col_min: isize = -1;
+            let mut col_max: isize = 1;
+
+            if row_index == 0 {
+                row_min = 0;
+            } else if row_index == seats[row_index].len() - 1 {
+                row_max = 0
+            }
+
+            if col_index == 0 {
+                col_min = 0;
+            } else if col_index == seats.len() - 1 {
+                col_max = 0
+            }
+
+            // println!("[{}, {}], row=[{}, {}], col=[{}, {}]", row_index, col_index, row_min, row_max, col_min, col_max);
+
+            for r in row_min..=row_max {
+                let line = seats.get((row_index as isize + r) as usize).unwrap();
+                for c in col_min..=col_max {
+                    if r == 0 && c == 0 {
+                        continue;
+                    }
+                    // println!("r={}, c={}, col_index={}", r, c, col_index);
+                    // dbg!(line);
+                    match line.get((col_index as isize + c) as usize).unwrap() {
+                        SeatState::Occuped => number_of_occuped += 1,
+                        _ => (),
+                    };
+                }
+            }
+
+            dbg!(number_of_occuped);
+
+            match number_of_occuped {
+                0 => SeatState::Occuped,
+                _ => SeatState::Empty,
+            }
+        },
+        SeatState::Occuped => {
+            // If 4 or more seats occupied => empty
+            let mut number_of_occuped = 0;
+            let mut row_min: isize = -1;
+            let mut row_max: isize = 1;
+            let mut col_min: isize = -1;
+            let mut col_max: isize = 1;
+
+            if row_index == 0 {
+                row_min = 0;
+            } else if row_index == seats[row_index].len() - 1 {
+                row_max = 0
+            }
+
+            if col_index == 0 {
+                col_min = 0;
+            } else if col_index == seats.len() - 1 {
+                col_max = 0
+            }
+
+            // println!("[{}, {}], row=[{}, {}], col=[{}, {}]", row_index, col_index, row_min, row_max, col_min, col_max);
+
+            for r in row_min..=row_max {
+                let line = seats.get((row_index as isize + r) as usize).unwrap();
+                for c in col_min..=col_max {
+                    if r == 0 && c == 0 {
+                        continue;
+                    }
+                    // println!("r={}, c={}, col_index={}", r, c, col_index);
+                    // dbg!(line);
+                    match line.get((col_index as isize + c) as usize).unwrap() {
+                        SeatState::Occuped => number_of_occuped += 1,
+                        _ => (),
+                    };
+                }
+            }
+
+            dbg!(number_of_occuped);
+
+            match number_of_occuped {
+                4..=8  => SeatState::Empty,
+                _ => SeatState::Occuped,
+            }
+        }
+    }
 }
 
 fn get_data() -> Vec<Vec<SeatState>> {
