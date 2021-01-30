efa0417476c4d813f09817aa897df0e695a535cd
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-01-30 22:47:47 +0100

    Day 17 part 1 OK

--- 2020/day_17/src/main.rs
+++ 2020/day_17/src/main.rs
@@ -1,67 +1,27 @@
-use std::{fs, io::SeekFrom, num::NonZeroUsize, os::macos::raw::stat};
+use std::{collections::HashSet, fmt, fs};
 
-#[derive(Debug)]
-enum State {
-    Active,
-    Inactive,
-}
-
-#[derive(Debug)]
+#[derive(Clone, PartialEq, Eq, Hash)]
 struct Cube {
-    x: usize,
-    y: usize,
-    state: State,
+    x: isize,
+    y: isize,
+    z: isize,
 }
 
 impl Cube {
-    fn new(x: usize, y: usize, state: char) -> Self {
-        let state = match state {
-            '#' => State::Active,
-            _ => State::Inactive,
-        };
-        Self {
-            x,
-            y,
-            state,
-        }
-    }
-
-    fn empty(x: usize, y: usize) -> Self {
-        Self {
-            x,
-            y,
-            state: State::Inactive,
-        }
+    fn new(x: isize, y: isize, z: isize) -> Self {
+        Self {x, y, z}
     }
 }
 
-#[derive(Debug)]
-struct Level {
-    z: isize,
-    cubes: Vec<Cube>,
-}
-
-impl Level {
-    fn new(z: isize, cubes: Vec<Cube>) -> Self {
-        Self {
-            z,
-            cubes,
-        }
+impl fmt::Display for Cube {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(x={}, y={}, z={})", self.x, self.y, self.z)
     }
 }
 
-#[derive(Debug)]
-struct PocketDimension {
-    levels: Vec<Level>,
-    height: usize,
-}
-
-impl PocketDimension {
-    fn new(level: Level) -> Self {
-        Self {
-            height: 1,
-            levels: vec![level],
-        }
+impl fmt::Debug for Cube {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Cube(x={}, y={}, z={})", self.x, self.y, self.z)
     }
 }
 
@@ -69,16 +29,95 @@ fn main() {
     println!("Part 1 result: {}", part1(get_data("input.txt")));
 }
 
-fn part1(initial_level: Level) -> usize {
-    let mut pocket_dimension = PocketDimension::new(initial_level);
-    dbg!(&pocket_dimension);
+fn part1(active_cubes: HashSet<Cube>) -> usize {
+    let mut min = Cube::new(isize::MAX, isize::MAX, isize::MAX);
+    let mut max = Cube::new(isize::MIN, isize::MIN, isize::MIN);
+    init_bounds(&active_cubes, &mut min, &mut max);
 
+    let mut active_cubes = active_cubes.clone();
+    
+    for _cycle in 1..=6 {
+        let mut cubes = active_cubes.clone();
 
-    0
+        for x in min.x - 1..=max.x + 1 {
+            for y in min.y - 1..=max.y + 1 {
+                for z in min.z - 1..=max.z + 1 {
+                    let cube = Cube::new(x, y, z);
+                    let active_neighbors = count_active_neighbors(&active_cubes, &cube);
+
+                    if cubes.contains(&cube) {
+                        if !(active_neighbors == 2 || active_neighbors == 3) {
+                            // Become inactive
+                            cubes.remove(&cube);
+                        }
+                    } else {
+                        if active_neighbors == 3 {
+                            // Become active
+                            cubes.insert(cube.clone());
+                            update_bounds(&cube, &mut min, &mut max);
+                        }
+                    }
+                }
+            }
+        }
+
+        active_cubes = cubes;
+
+    }
+
+    active_cubes.len()
 }
 
-fn get_data(file: &str) -> Level {
-    let cubes: Vec<Cube> = fs::read_to_string(file).expect("Cannot read file")
+fn count_active_neighbors(active_cubes: &HashSet<Cube>, cube: &Cube) -> usize {
+    let mut count = 0;
+
+    for x in -1..=1 {
+        for y in -1..=1 {
+            for z in -1..=1 {
+                // Cube itself
+                if x == 0 && y == 0 && z == 0 {
+                    continue;
+                }
+
+                if active_cubes.contains(&Cube::new(cube.x + x, cube.y + y, cube.z + z)) {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    count
+}
+
+fn init_bounds(cubes: &HashSet<Cube>, min: &mut Cube, max: &mut Cube) {
+    for cube in cubes {
+        update_bounds(&cube, min, max);
+    }
+}
+
+fn update_bounds(cube: &Cube, mut min: &mut Cube, mut max: &mut Cube) {
+    if cube.x < min.x {
+        min.x = cube.x;
+    }
+    if cube.y < min.y {
+        min.y = cube.y;
+    }
+    if cube.z < min.z {
+        min.z = cube.z;
+    }
+    if cube.x > max.x {
+        max.x = cube.x;
+    }
+    if cube.y > max.y {
+        max.y = cube.y;
+    }
+    if cube.z > max.z {
+        max.z = cube.z;
+    }
+}
+
+fn get_data(file: &str) -> HashSet<Cube> {
+    fs::read_to_string(file).expect("Cannot read file")
         .trim()
         .lines()
         .enumerate()
@@ -87,18 +126,17 @@ fn get_data(file: &str) -> Level {
                 .trim()
                 .chars()
                 .enumerate()
-                .map(|(x, c)| {
-                    Cube::new(x, y, c)
+                .filter(|(_, c)| *c == '#')
+                .map(|(x, _)| {
+                    Cube::new(x as isize, y as isize, 0)
                 })
-                .collect::<Vec<Cube>>()
+                .collect::<HashSet<Cube>>()
         })
-        .collect();
-
-    Level::new(0, cubes)
+        .collect()
 }
 
 #[test]
 fn test_part1() {
     assert_eq!(112, part1(get_data("test.txt")));
-    // assert_eq!(, part1(get_data("input.txt")));
+    assert_eq!(313, part1(get_data("input.txt")));
 }
