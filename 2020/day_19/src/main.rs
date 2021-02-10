b82e0807092db51b691e9485adb365d6c15f412c
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-02-10 13:40:44 +0100

    Pas grand chose

--- 2020/day_19/src/main.rs
+++ 2020/day_19/src/main.rs
@@ -19,11 +19,36 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Cannot find input.txt");
 
     println!("Part 1 result: {}", part1(get_data(&input)));
-    // println!("Part 2 result: {}", part2(get_data(&input)));
+    println!("Part 2 result: {}", part2(get_data(&input)));
 }
 
 fn part1(data: (HashMap<usize, Rule>, Vec<String>)) -> usize {
     let possible_messages = get_all_messages(&data.0, 0, &vec![]);
+
+    let mut result = 0;
+    let messages = data.1;
+
+    for message in messages {
+        if possible_messages.contains(&message) {
+            result += 1;
+        }
+    }
+
+    result
+}
+
+fn part2(data: (HashMap<usize, Rule>, Vec<String>)) -> usize {
+    let mut rules = data.0;
+    rules.insert(8, Rule {
+        val: String::from(""),
+        rules: vec![vec![42], vec![42, 8]],
+    });
+    rules.insert(11, Rule {
+        val: String::from(""),
+        rules: vec![vec![42, 31], vec![42, 11, 31]],
+    });
+
+    let possible_messages = get_all_messages(&rules, 0, &vec![]);
 
     let mut result = 0;
     let messages = data.1;
@@ -186,8 +211,8 @@ aaabbb
 aaaabbb"#;
     assert_eq!(2, part1(get_data(input)));
 
-    let input = fs::read_to_string("input.txt").expect("Cannot find input.txt");
-    assert_eq!(182, part1(get_data(&input)));
+    // let input = fs::read_to_string("input.txt").expect("Cannot find input.txt");
+    // assert_eq!(182, part1(get_data(&input)));
 }
 
 #[test]
@@ -221,4 +246,60 @@ fn test_parse_rules_line() {
         },
         rule
     );
+}
+
+#[test]
+fn test_part2() {
+    let input = r#"
+42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba"#;
+    assert_eq!(12, part2(get_data(input)));
+
+    // let input = fs::read_to_string("input.txt").expect("Cannot find input.txt");
+    // assert_eq!(182, part1(get_data(&input)));
 }
