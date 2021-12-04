7c2bcb830dd2be807205e86701e7f9089f5ab0a0
Fabien <fabienbellanger@users.noreply.github.com>
2021-12-04 15:20:27 +0100

    Improve day 2

--- 2021/day_02/src/main.rs
+++ 2021/day_02/src/main.rs
@@ -1,16 +1,30 @@
 use std::fs;
+use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Direction {
-    Forward,
-    Up,
-    Down,
+    Forward(isize),
+    Up(isize),
+    Down(isize),
 }
 
 #[derive(Debug)]
-struct Movement {
-    direction: Direction,
-    units: isize,
+struct ParseDirectionError {}
+
+impl FromStr for Direction {
+    type Err = ParseDirectionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.split_once(' ') {
+            None => Err(Self::Err {}),
+            Some(s) => match (s.0, s.1.parse().unwrap()) {
+                ("forward", value) => Ok(Direction::Forward(value)),
+                ("down", value) => Ok(Direction::Down(value)),
+                ("up", value) => Ok(Direction::Up(value)),
+                _ => Err(Self::Err {}),
+            },
+        }
+    }
 }
 
 #[derive(Default)]
@@ -25,94 +39,63 @@ fn main() {
     println!("Part 2 result: {}", part2(get_data()));
 }
 
-fn part1(lines: Vec<Movement>) -> isize {
+fn part1(lines: Vec<Direction>) -> isize {
     let mut position = Position::default();
 
     for line in lines {
-        match line.direction {
-            Direction::Forward => position.horizontal += line.units,
-            Direction::Down => position.depth += line.units,
-            Direction::Up => position.depth -= line.units,
+        match line {
+            Direction::Forward(value) => position.horizontal += value,
+            Direction::Down(value) => position.depth += value,
+            Direction::Up(value) => position.depth -= value,
         }
     }
 
     position.depth * position.horizontal
 }
 
-fn part2(lines: Vec<Movement>) -> isize {
+fn part2(lines: Vec<Direction>) -> isize {
     let mut position = Position::default();
 
     for line in lines {
-        match line.direction {
-            Direction::Forward => {
-                position.horizontal += line.units;
-                position.depth += position.aim * line.units;
+        match line {
+            Direction::Forward(value) => {
+                position.horizontal += value;
+                position.depth += position.aim * value;
             }
-            Direction::Down => position.aim += line.units,
-            Direction::Up => position.aim -= line.units,
+            Direction::Down(value) => position.aim += value,
+            Direction::Up(value) => position.aim -= value,
         }
     }
 
     position.depth * position.horizontal
 }
+
+const _DIRECTIONS_TEST: [Direction; 6] = [
+    Direction::Forward(5),
+    Direction::Down(5),
+    Direction::Forward(8),
+    Direction::Up(3),
+    Direction::Down(8),
+    Direction::Forward(2),
+];
 
 #[test]
 fn test_part1() {
-    assert_eq!(150, part1(_movements_test()));
+    assert_eq!(150, part1(_DIRECTIONS_TEST.to_vec()));
     assert_eq!(1507611, part1(get_data()));
 }
 
 #[test]
 fn test_part2() {
-    assert_eq!(900, part2(_movements_test()));
+    assert_eq!(900, part2(_DIRECTIONS_TEST.to_vec()));
     assert_eq!(1880593125, part2(get_data()));
 }
 
-fn _movements_test() -> Vec<Movement> {
-    vec![
-        Movement {
-            direction: Direction::Forward,
-            units: 5,
-        },
-        Movement {
-            direction: Direction::Down,
-            units: 5,
-        },
-        Movement {
-            direction: Direction::Forward,
-            units: 8,
-        },
-        Movement {
-            direction: Direction::Up,
-            units: 3,
-        },
-        Movement {
-            direction: Direction::Down,
-            units: 8,
-        },
-        Movement {
-            direction: Direction::Forward,
-            units: 2,
-        },
-    ]
-}
-
-fn get_data() -> Vec<Movement> {
+fn get_data() -> Vec<Direction> {
     fs::read_to_string("input.txt")
         .expect("Cannot read the file input.txt")
         .trim()
         .lines()
-        .map(|line| {
-            let mut parts = line.split(' ');
-            let direction = match parts.next().unwrap() {
-                "forward" => Direction::Forward,
-                "down" => Direction::Down,
-                "up" => Direction::Up,
-                _ => panic!("invalid direction"),
-            };
-            let units = parts.next().unwrap().parse().unwrap();
-
-            Movement { direction, units }
-        })
+        .map(|line| line.parse().unwrap())
         .collect()
 }
