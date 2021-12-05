83a5e3c28b4b417105ad51b09a794ad6a909ff73
Fabien <fabienbellanger@users.noreply.github.com>
2021-12-05 18:27:53 +0100

    Day 4 OK

--- 2021/day_04/src/main.rs
+++ 2021/day_04/src/main.rs
@@ -2,47 +2,181 @@ use std::fs;
 
 const BINGO_SIZE: usize = 5;
 
-type Board = Vec<Vec<(u8, bool)>>;
+#[derive(Debug, Clone, PartialEq)]
+struct Board {
+    board: Vec<Vec<(u8, bool)>>,
+}
 
-#[derive(Debug)]
+impl Board {
+    fn update(&mut self, n: u8) {
+        for row in self.board.iter_mut() {
+            for item in row.iter_mut() {
+                if item.0 == n && !item.1 {
+                    item.1 = true;
+                }
+            }
+        }
+    }
+
+    fn winned(&self) -> bool {
+        // Rows?
+        let mut win = false;
+        'r: for row in &self.board {
+            for item in row {
+                if !item.1 {
+                    continue 'r;
+                }
+            }
+            // No item equal to false, so it's ok
+            win = true;
+            break;
+        }
+
+        if !win {
+            // Columns?
+            'c: for row in 0..BINGO_SIZE {
+                for col in 0..BINGO_SIZE {
+                    if !self.board[col][row].1 {
+                        continue 'c;
+                    }
+                }
+                win = true;
+                break;
+            }
+        }
+
+        win
+    }
+}
+
+#[derive(Debug, Default)]
 struct Game {
     numbers: Vec<u8>,
     boards: Vec<Board>,
 }
 
-impl Default for Game {
-    fn default() -> Self {
-        Self {
-            numbers: vec![],
-            boards: vec![],
-        }
-    }
-}
-
 fn main() {
     println!("Part 1 result: {}", part1(get_data("input.txt")));
-    // println!("Part 2 result: {}", part2(get_data("input.txt")));
+    println!("Part 2 result: {}", part2(get_data("input.txt")));
 }
 
 fn part1(game: Game) -> usize {
-    dbg!(&game);
-    0
+    let mut boards = game.boards.clone();
+    let mut winner_sum: usize = 0;
+    let mut winner_number: usize = 0;
+
+    'win: for number in game.numbers {
+        for board in &mut boards {
+            board.update(number);
+            if board.winned() {
+                winner_number = number as usize;
+                for row in &board.board {
+                    for item in row {
+                        if !item.1 {
+                            winner_sum += item.0 as usize;
+                        }
+                    }
+                }
+                break 'win;
+            }
+        }
+    }
+
+    winner_number * winner_sum
 }
 
-fn part2(lines: Vec<String>) -> usize {
-    0
+fn part2(game: Game) -> usize {
+    let mut boards = game.boards.clone();
+    let mut winner_boards: Vec<Board> = vec![];
+    let mut winner_sum: usize = 0;
+    let mut winner_number: usize = 0;
+
+    for number in game.numbers {
+        for board in &mut boards {
+            if !winner_boards.contains(board) {
+                board.update(number);
+                if board.winned() {
+                    winner_number = number as usize;
+                    winner_sum = 0;
+                    for row in &board.board {
+                        for item in row {
+                            if !item.1 {
+                                winner_sum += item.0 as usize;
+                            }
+                        }
+                    }
+
+                    winner_boards.push(board.clone());
+                }
+            }
+        }
+    }
+
+    winner_number * winner_sum
+}
+
+#[test]
+fn test_winner_board_row() {
+    let mut board = Board {
+        board: vec![
+            vec![(1, false), (4, false), (6, true), (9, false), (9, false)],
+            vec![(12, true), (14, true), (16, false), (19, true), (19, true)],
+            vec![
+                (21, false),
+                (24, false),
+                (26, true),
+                (29, false),
+                (29, false),
+            ],
+            vec![
+                (31, false),
+                (34, false),
+                (36, true),
+                (39, false),
+                (39, false),
+            ],
+            vec![
+                (41, false),
+                (44, false),
+                (46, true),
+                (49, false),
+                (49, false),
+            ],
+        ],
+    };
+    assert_eq!(false, board.winned());
+
+    board.update(16);
+    assert_eq!(true, board.winned());
+}
+
+#[test]
+fn test_winner_board_column() {
+    let mut board = Board {
+        board: vec![
+            vec![(1, false), (4, false), (6, true), (9, false), (9, false)],
+            vec![(12, true), (14, true), (16, false), (19, true), (19, true)],
+            vec![(21, true), (24, false), (26, true), (29, false), (29, true)],
+            vec![(31, true), (34, false), (36, true), (39, false), (39, true)],
+            vec![(41, true), (44, false), (46, true), (49, false), (49, true)],
+        ],
+    };
+    assert_eq!(false, board.winned());
+
+    board.update(9);
+    assert_eq!(true, board.winned());
 }
 
 #[test]
 fn test_part1() {
     assert_eq!(4512, part1(get_data("test.txt")));
-    // assert_eq!(3895776, part1(get_data("input.txt")));
+    assert_eq!(21607, part1(get_data("input.txt")));
 }
 
 #[test]
 fn test_part2() {
-    // assert_eq!(230, part2(get_data("test.txt")));
-    // assert_eq!(7928162, part2(get_data("input.txt")));
+    assert_eq!(1924, part2(get_data("test.txt")));
+    assert_eq!(19012, part2(get_data("input.txt")));
 }
 
 fn get_data(file: &str) -> Game {
@@ -72,7 +206,7 @@ fn get_data(file: &str) -> Game {
                         .collect::<Vec<(u8, bool)>>()
                 })
                 .collect();
-            board
+            Board { board }
         })
         .collect();
 
