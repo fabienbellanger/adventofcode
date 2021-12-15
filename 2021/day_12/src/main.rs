6504492b30907c334d077eb1eb90fb051c3e5378
Fabien <fabienbellanger@users.noreply.github.com>
2021-12-15 08:48:21 +0100

    Continue day 12

--- 2021/day_12/src/main.rs
+++ 2021/day_12/src/main.rs
@@ -1,13 +1,65 @@
-use std::{collections::HashMap, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 fn main() {
     println!("Part 1 result: {}", part1(get_data("input.txt")));
     // println!("Part 2 result: {}", part2(get_data("input.txt")));
 }
 
+fn is_lower(s: &String) -> bool {
+    s.chars().all(|c| c.is_lowercase())
+}
+
+fn find_path(
+    paths: &HashMap<String, Vec<String>>,
+    current: String,
+    visited: &mut HashSet<String>,
+    path: &mut Vec<String>,
+) -> usize {
+    /*
+    start,A,b,A,c,A,end
+    start,A,b,A,end
+    X start,A,b,end
+    start,A,c,A,b,A,end
+    start,A,c,A,b,end
+    start,A,c,A,end
+    start,A,end
+    start,b,A,c,A,end
+    start,b,A,end
+    start,b,end
+    */
+
+    path.push(current.clone());
+
+    if current == "end".to_owned() {
+        println!("======> {:?}", &path);
+        return 1;
+    }
+
+    let mut count = 0;
+    for next in paths.get(&current).unwrap() {
+        // println!("{} {:?}", next, visited);
+        if is_lower(next) {
+            if visited.contains(next) {
+                continue;
+            } else {
+                visited.insert(next.clone());
+            }
+        }
+
+        count += find_path(&paths, next.clone(), &mut visited.clone(), &mut path.clone());
+    }
+
+    count
+}
+
 fn part1(paths: HashMap<String, Vec<String>>) -> usize {
     dbg!(&paths);
-    0
+    let mut visited = HashSet::new();
+    visited.insert("start".to_string());
+    find_path(&paths, "start".to_string(), &mut visited, &mut Vec::new())
 }
 
 fn part2() -> usize {
@@ -37,7 +89,6 @@ fn get_data(file: &str) -> HashMap<String, Vec<String>> {
             let (from, to) = line.split_once('-').unwrap();
 
             paths.entry(from.to_string()).or_default().push(to.to_string());
-
             paths.entry(to.to_string()).or_default().push(from.to_string());
         });
 
