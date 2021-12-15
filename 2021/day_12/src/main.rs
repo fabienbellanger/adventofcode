23b48576118f46c2f642f3bc2c51f7a4098f4298
Fabien Bellanger <fabien.bellanger@apitic.com>
2021-12-15 12:43:48 +0100

    Day 12 OK

--- 2021/day_12/src/main.rs
+++ 2021/day_12/src/main.rs
@@ -5,77 +5,73 @@ use std::{
 
 fn main() {
     println!("Part 1 result: {}", part1(get_data("input.txt")));
-    // println!("Part 2 result: {}", part2(get_data("input.txt")));
+    println!("Part 2 result: {}", part2(get_data("input.txt")));
 }
 
-fn is_lower(s: &String) -> bool {
-    s.chars().all(|c| c.is_lowercase())
+fn is_small_cave(s: &str) -> bool {
+    s.chars().next().unwrap().is_lowercase()
 }
 
 fn find_path(
     paths: &HashMap<String, Vec<String>>,
     current: String,
-    visited: &mut HashSet<String>,
+    visited: HashSet<String>,
     path: &mut Vec<String>,
+    twice: bool,
 ) -> usize {
-    /*
-    start,A,b,A,c,A,end
-    start,A,b,A,end
-    X start,A,b,end
-    start,A,c,A,b,A,end
-    start,A,c,A,b,end
-    start,A,c,A,end
-    start,A,end
-    start,b,A,c,A,end
-    start,b,A,end
-    start,b,end
-    */
-
     path.push(current.clone());
 
-    if current == "end".to_owned() {
-        println!("======> {:?}", &path);
+    if current == *"end" {
+        // println!("===> {:?}", &path);
         return 1;
     }
 
     let mut count = 0;
     for next in paths.get(&current).unwrap() {
-        // println!("{} {:?}", next, visited);
-        if is_lower(next) {
+        let mut visited = visited.clone();
+        let mut twice = twice;
+        if is_small_cave(next) {
             if visited.contains(next) {
-                continue;
+                if !twice || next == "start" || next == "end" {
+                    continue;
+                } else {
+                    twice = false;
+                }
             } else {
                 visited.insert(next.clone());
             }
         }
 
-        count += find_path(&paths, next.clone(), &mut visited.clone(), &mut path.clone());
+        count += find_path(paths, next.clone(), visited, &mut path.clone(), twice);
     }
 
     count
 }
 
 fn part1(paths: HashMap<String, Vec<String>>) -> usize {
-    dbg!(&paths);
     let mut visited = HashSet::new();
     visited.insert("start".to_string());
-    find_path(&paths, "start".to_string(), &mut visited, &mut Vec::new())
+
+    find_path(&paths, "start".to_string(), visited, &mut Vec::new(), false)
 }
 
-fn part2() -> usize {
-    0
+fn part2(paths: HashMap<String, Vec<String>>) -> usize {
+    let mut visited = HashSet::new();
+    visited.insert("start".to_string());
+
+    find_path(&paths, "start".to_string(), visited, &mut Vec::new(), true)
 }
 
 #[test]
 fn test_part1() {
     assert_eq!(10, part1(get_data("test.txt")));
-    // assert_eq!(344297, part1(get_data("input.txt")));
+    assert_eq!(4912, part1(get_data("input.txt")));
 }
 
 #[test]
 fn test_part2() {
-    // assert_eq!(168, part2(get_data("test.txt")));
-    // assert_eq!(97164301, part2(get_data("input.txt")));
+    assert_eq!(36, part2(get_data("test.txt")));
+    assert_eq!(150004, part2(get_data("input.txt")));
 }
 
 fn get_data(file: &str) -> HashMap<String, Vec<String>> {
