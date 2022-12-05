d96b10cff32099d1742a0cb924e4282d7b42bdd2
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-12-05 08:55:52 +0100

    Improve day 1

--- 2022/day_01/src/main.rs
+++ 2022/day_01/src/main.rs
@@ -17,20 +17,19 @@ fn part2(data: Vec<Vec<usize>>) -> usize {
         .map(|elf| elf.iter().sum::<usize>())
         .collect();
     elves.sort_unstable();
-    elves.reverse();
-    elves.iter().take(3).sum()
+    elves.iter().rev().take(3).sum()
 }
 
 #[test]
 fn test_part1() {
     assert_eq!(24_000, part1(get_data("test.txt")));
-    assert_eq!(1139, part1(get_data("input.txt")));
+    assert_eq!(72_602, part1(get_data("input.txt")));
 }
 
 #[test]
 fn test_part2() {
     assert_eq!(45_000, part2(get_data("test.txt")));
-    assert_eq!(1139, part2(get_data("input.txt")));
+    assert_eq!(207_410, part2(get_data("input.txt")));
 }
 
 fn get_data(file: &str) -> Vec<Vec<usize>> {
@@ -40,9 +39,8 @@ fn get_data(file: &str) -> Vec<Vec<usize>> {
     parts
         .trim()
         .split("\n\n")
-        .into_iter()
-        .map(|part| part.trim().split('\n').into_iter()
-            .map(|line| line.parse::<usize>().unwrap())
+        .map(|part| part.trim().split('\n')
+            .map(|line| line.parse::<usize>().unwrap_or_default())
             .collect::<Vec<usize>>())
         .collect()
 }
