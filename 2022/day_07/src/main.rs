85809f7e5fd9f505aee1a80b3d3a47d1eb882aa3
Fabien Bellanger <fabien.bellanger@apitic.com>
2022-12-08 18:16:31 +0100

    Day 7 part 1 OK

--- 2022/day_07/src/main.rs
+++ 2022/day_07/src/main.rs
@@ -1,9 +1,76 @@
-use std::{fs, collections::{HashMap}};
+/*use std::collections::{HashMap, HashSet};
+
+fn main() {
+    let input = include_str!("../input.txt");
+    println!("Part 1: {:?}", navigate(input));
+}
+
+fn cd(i: &str, cur:&mut Vec<String>) {
+    match i {
+        ".." => { cur.pop(); },
+        "/" => { cur.clear(); },
+        _ => {   cur.push(i.to_string()); }
+    };
+}
+
+fn dir_size(from: &str, dirs: &HashMap<String, HashSet<(&str, usize)>>, counted: &mut HashSet<String>) -> usize {
+    let mut s = dirs.get(from).unwrap().iter().filter_map(|&(f, size)| {
+        let full_path = [from, f].join("");
+        let n = (!counted.contains(&full_path)).then(|| size);
+        counted.insert(full_path);
+        n
+    }).sum();
+
+    dirs.keys()
+        .filter(|l| l.starts_with(from))
+        .filter(|&p| p != from)
+        .for_each(|d| s += dir_size(d, dirs, counted));
+    s
+}
+
+
+fn navigate(s: &str) -> (usize, usize) {
+    let mut dir = vec![];
+    let mut dirs: HashMap<String, HashSet<(&str, usize)>> = HashMap::new();
+
+    for line in s.lines() {
+        if line.starts_with("$") {
+            let splits: Vec<_> = line.split_whitespace().collect();
+            match splits[1] {
+                "ls" => (),
+                "cd" => cd(splits[2], &mut dir),
+                _ => panic!()
+            };
+        } else {
+            let current_dir = dir.join("/");
+            if !dirs.contains_key(&current_dir) {
+                dirs.insert(current_dir.clone(), HashSet::new());
+            }
+
+            if !line.starts_with("dir") {
+                let (fsize, fname) =  line.split_once(' ').unwrap();
+                let fsize = fsize.parse().unwrap();
+                dirs.get_mut(&current_dir).unwrap().insert((fname, fsize));
+            }
+        }
+    }
+
+    dbg!(&dirs);
+    
+    let sizes: Vec<_> = dirs.keys()
+        .map(|k| dir_size(k, &dirs, &mut HashSet::new()) )
+        .collect();
+
+    let delta = 30_000_000 + sizes.iter().max().unwrap() - 70_000_000;
+    (sizes.iter().filter(|&&n| n <= 100_000).sum(),
+        *sizes.iter().filter(|&&n| n >= delta).min().unwrap())
+}*/
+
+use std::{fs, collections::{BTreeMap}};
 
 #[derive(Debug)]
 enum Output {
     Command(Command),
-    Directory(String),
     File(String, usize),
 }
 
@@ -14,44 +81,34 @@ enum Command {
     CdChild(String),
 }
 
-#[derive(Debug)]
-struct File {
-    name: String,
-    size: usize,
-}
-
-fn build_outputs(outputs: Vec<Output>) -> HashMap<String, (Vec<File>, usize)> {
-    let mut result: HashMap<String, (Vec<File>, usize)> = HashMap::new();
+fn build_outputs(outputs: Vec<Output>) -> BTreeMap<String, usize> {
+    let mut result: BTreeMap<String, usize> = BTreeMap::new();
     let mut current = String::new();
 
     for output in outputs {
         match output {
-            Output::File(name, size) => {
-                if let Some((r, t)) = result.get_mut(&current) {
-                    r.push(File{name, size});
-                    *t = *t + size;
+            Output::File(_name, size) => {
+                if let Some(s) = result.get_mut(&current) {
+                    *s = *s + size;
                 }
             },
-            Output::Directory(_name) => (),
             Output::Command(cmd) => match cmd {
                 Command::CdRoot => {
                     current = "/".to_owned();
-                    result.insert(current.clone(), (vec![], 0));
+                    result.insert(current.clone(), 0);
                 },
                 Command::CdParent => {
                     if &current != "/" {
                         let mut parts = current.split_terminator('/').collect::<Vec<_>>();
                         parts.pop();
                         current = parts.join("/");
-                        if current.is_empty() {
-                            current = "/".to_owned();
-                        }
+                        current.push('/');
                     }
                 },
                 Command::CdChild(dir) => {
                     current.push_str(&dir);
                     current.push_str("/");
-                    result.insert(current.clone(), (vec![], 0));
+                    result.insert(current.clone(), 0);
                 },
             }
         }
@@ -68,9 +125,25 @@ fn main() {
 fn part1(data: Vec<Output>) -> usize {
     let directories = build_outputs(data);
     let keys = directories.keys().collect::<Vec<_>>();
-    dbg!(&directories, &keys);
 
-    0
+    let mut result = 0;
+    for (name, _size) in directories.iter() {
+        let mut sum = 0;
+
+        // Find sub directories
+        for sd in keys.iter() {
+            if sd.starts_with(name) {
+                let size_sub = directories.get(*sd).unwrap();
+                sum += size_sub;
+            }
+        }
+
+        if sum <= 100_000 {
+            result += sum;
+        }
+    }
+
+    result
 }
 
 // fn part2(data: Vec<Output>) -> usize {
@@ -80,7 +153,7 @@ fn part1(data: Vec<Output>) -> usize {
 #[test]
 fn test_part1() {
     assert_eq!(95437, part1(get_data("test.txt")));
-    // assert_eq!(0, part1(get_data("input.txt")));
+    assert_eq!(1325919, part1(get_data("input.txt")));
 }
 
 // #[test]
@@ -103,8 +176,7 @@ fn get_data(file: &str) -> Vec<Output> {
                     _ => Command::CdChild(name.to_string())
                 }))
             } else if line.starts_with("dir ") {
-                let (_, name) = line.split_once(' ').unwrap();
-                Some(Output::Directory(name.to_string()))
+                None
             } else if !line.starts_with("$ ls"){
                 let (size, name) = line.split_once(' ').unwrap();
                 Some(Output::File(name.to_string(), size.parse::<usize>().unwrap_or_default()))
