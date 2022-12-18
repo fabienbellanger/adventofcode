f478794d23ec668843bec361b1ba2e137a681f92
Fabien <fabienbellanger@users.noreply.github.com>
2022-12-18 22:59:40 +0100

    Start day 10

--- 2022/day_10/src/main.rs
+++ 2022/day_10/src/main.rs
@@ -1,16 +1,39 @@
-use std::fs;
+use std::{fs, result};
+
+#[derive(Debug)]
+enum Instruction {
+    Noop, // 1 cycle
+    Addx(isize), // 2 cycles
+}
 
 fn main() {
     println!("Part 1 result: {}", part1(get_data("input.txt")));
     // println!("Part 2 result: {}", part2(get_data("input.txt")));
 }
 
-fn part1(data: Vec<String>) -> usize {
+fn part1(data: Vec<Instruction>) -> usize {
+    // cycles = 20 | 60 | 100 | 140 | 180 | 220
+    // sum = v * 20 + k * 60 + j * 100 + i * 140 + m * 180 + q * 220
     dbg!(&data);
-    0
+    let mut result = 0;
+    let mut register = 1;
+    let mut cycles = 0;
+
+    for i in data {
+        match i {
+            Instruction::Noop => {
+
+            },
+            Instruction::Addx(v) => {
+
+            },
+        }
+    }
+    
+    result
 }
 
-// fn part2(data: Vec<String>) -> usize {
+// fn part2(data: Vec<Instruction>) -> usize {
 //     0
 // }
 
@@ -26,11 +49,18 @@ fn test_part1() {
 //     // assert_eq!(0, part2(get_data("input.txt")));
 // }
 
-fn get_data(file: &str) -> Vec<String> {
+fn get_data(file: &str) -> Vec<Instruction> {
     fs::read_to_string(file)
         .expect("Cannot read the file input.txt")
         .trim()
         .lines()
-        .map(|line| line.to_string())
+        .map(|line| 
+            if line == "noop" {
+                Instruction::Noop
+            } else {
+                let (_, value) = line.split_once(' ').unwrap();
+                Instruction::Addx(value.parse().unwrap())
+            }
+        )
         .collect()
 }
