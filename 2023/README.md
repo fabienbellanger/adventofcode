9828a6b9f13c0125d1da558e4cfd3c500f1edc32
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-12-01 08:52:10 +0100

    Préparation Advent of code 2023

---
+++ 2023/README.md
@@ -0,0 +1,29 @@
+# Advent Of Code 2023
+
+My [adventofcode.com](https://adventofcode.com/2023) solutions
+
+- [ ] Day 01: Rust (`cargo run --bin day_01`)
+- [ ] Day 02: Rust
+- [ ] Day 03: Rust
+- [ ] Day 04: Rust
+- [ ] Day 05: Rust
+- [ ] Day 06: Rust
+- [ ] Day 07: Rust
+- [ ] Day 08: Rust
+- [ ] Day 09: Rust
+- [ ] Day 10: Rust
+- [ ] Day 11: Rust
+- [ ] Day 12: Rust
+- [ ] Day 13: Rust
+- [ ] Day 14: Rust
+- [ ] Day 15: Rust
+- [ ] Day 16: Rust
+- [ ] Day 17: Rust
+- [ ] Day 18: Rust
+- [ ] Day 19: Rust
+- [ ] Day 20: Rust
+- [ ] Day 21: Rust
+- [ ] Day 22: Rust
+- [ ] Day 23: Rust
+- [ ] Day 24: Rust
+- [ ] Day 25: Rust
---
+++ 2023/day_/src/main.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+fn main() {
+    println!("Part 1 result: {}", part1(get_data("input.txt")));
+    // println!("Part 2 result: {}", part2(get_data("input.txt")));
+}
+
+fn part1(data: Vec<String>) -> usize {
+    dbg!(&data);
+    0
+}
+
+// fn part2(data: Vec<String>) -> usize {
+//     0
+// }
+
+#[test]
+fn test_part1() {
+    assert_eq!(0, part1(get_data("test.txt")));
+    // assert_eq!(0, part1(get_data("input.txt")));
+}
+
+// #[test]
+// fn test_part2() {
+//     assert_eq!(0, part2(get_data("test.txt")));
+//     // assert_eq!(0, part2(get_data("input.txt")));
+// }
+
+fn get_data(file: &str) -> Vec<String> {
+    fs::read_to_string(file)
+        .expect("Cannot read the file input.txt")
+        .trim()
+        .lines()
+        .map(|line| line.to_string())
+        .collect()
+}
