e0c4e324caedb48677283b6a7d0bc0e967113019
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-12-05 12:27:12 +0100

    Day 3 OK

--- 2023/day_03/src/main.rs
+++ 2023/day_03/src/main.rs
@@ -1,7 +1,6 @@
 #![allow(unused_variables)]
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::{fmt, fs};
-use utils::data::file_to_vec_string;
 
 const INPUT: &str = "input.txt";
 
@@ -34,7 +33,7 @@ impl Point {
     }
 }
 
-type Symbols = HashSet<Point>;
+type Symbols = HashMap<Point, char>;
 
 #[derive(Debug, Default, Clone)]
 struct PartNumber {
@@ -49,7 +48,6 @@ impl fmt::Display for PartNumber {
 }
 
 impl PartNumber {
-    // TODO: Add tests
     fn neighbors(&self) -> HashSet<Point> {
         let mut list = HashSet::new();
 
@@ -61,11 +59,21 @@ impl PartNumber {
 
         list
     }
+
+    fn points(&self) -> Vec<Point> {
+        let mut points = vec![self.origin.clone()];
+
+        for x in 1..self.value.len() {
+            points.push(Point::new(self.origin.x + x as isize, self.origin.y));
+        }
+
+        points
+    }
 }
 
 fn main() {
     println!("Part 1 result: {}", part1(parse_input(INPUT)));
-    println!("Part 2 result: {}", part2(file_to_vec_string(INPUT)));
+    println!("Part 2 result: {}", part2(parse_input(INPUT)));
 }
 
 fn part1(data: (Vec<PartNumber>, Symbols)) -> usize {
@@ -74,30 +82,65 @@ fn part1(data: (Vec<PartNumber>, Symbols)) -> usize {
 
     part_numbers
         .iter()
-        .inspect(|s| println!("{s}"))
         .filter_map(
-            |part_number| match part_number.neighbors().iter().any(|point| symbols.contains(point)) {
+            |part_number| match part_number.neighbors().iter().any(|point| symbols.get(point).is_some()) {
                 false => None,
                 true => Some(part_number.value.parse::<usize>().unwrap_or_default()),
             },
         )
-        .inspect(|s| println!("  => {s}"))
         .sum()
 }
 
-fn part2(data: Vec<String>) -> u32 {
-    todo!()
+fn part2(data: (Vec<PartNumber>, Symbols)) -> usize {
+    let part_numbers = data.0;
+    let gears: HashSet<Point> = data
+        .1
+        .iter()
+        .filter_map(|(point, c)| if *c == '*' { Some(point) } else { None })
+        .cloned()
+        .collect();
+
+    gears
+        .into_iter()
+        .filter_map(|gear| {
+            let mut adjacents: Vec<usize> = vec![];
+
+            for number in part_numbers.iter() {
+                let neighbors = gear.neighbors();
+                let points = number.points();
+                let mut is_adjacent = false;
+
+                for point in points {
+                    if neighbors.contains(&point) {
+                        is_adjacent = true;
+                        break;
+                    }
+                }
+
+                if is_adjacent {
+                    adjacents.push(number.value.parse().unwrap_or_default());
+                }
+            }
+
+            if adjacents.len() == 2 {
+                Some(adjacents.into_iter().product::<usize>())
+            } else {
+                None
+            }
+        })
+        .sum()
 }
 
 fn parse_input(file: &str) -> (Vec<PartNumber>, Symbols) {
     let lines = fs::read_to_string(file).expect("Cannot read the file input.txt");
 
     let mut part_numbers = vec![];
-    let mut symbols = HashSet::new();
+    let mut symbols = HashMap::new();
 
     for (y, line) in lines.trim().lines().enumerate() {
         let mut current_number = String::new();
         let mut current_origin = Point::new(0, y as isize);
+        let line_size = line.len();
 
         for (x, c) in line.chars().enumerate() {
             if c.is_ascii_digit() {
@@ -105,18 +148,29 @@ fn parse_input(file: &str) -> (Vec<PartNumber>, Symbols) {
                     current_origin.x = x as isize;
                 }
                 current_number.push(c);
-            } else if !current_number.is_empty() {
-                let part_number = PartNumber {
-                    value: current_number.clone(),
-                    origin: current_origin.clone(),
-                };
-                part_numbers.push(part_number);
 
-                current_number.clear();
-            }
+                // Last char of the line
+                if x == line_size - 1 {
+                    let part_number = PartNumber {
+                        value: current_number.clone(),
+                        origin: current_origin.clone(),
+                    };
+                    part_numbers.push(part_number);
+                }
+            } else {
+                if c != '.' {
+                    symbols.insert(Point::new(x as isize, y as isize), c);
+                }
 
-            if !c.is_ascii_digit() && c != '.' {
-                symbols.insert(Point::new(x as isize, y as isize));
+                if !current_number.is_empty() {
+                    let part_number = PartNumber {
+                        value: current_number.clone(),
+                        origin: current_origin.clone(),
+                    };
+                    part_numbers.push(part_number);
+
+                    current_number.clear();
+                }
             }
         }
     }
@@ -133,13 +187,13 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_eq!(4361, part1(parse_input(TEST)));
-        // assert_eq!(0, part1(parse_input(INPUT)));
+        assert_eq!(543867, part1(parse_input(INPUT)));
     }
 
     #[test]
     fn test_part2() {
-        // assert_eq!(0, part2(file_to_vec_string(TEST)));
-        // assert_eq!(0, part2(file_to_vec_string(INPUT)));
+        assert_eq!(467_835, part2(parse_input(TEST)));
+        assert_eq!(79_613_331, part2(parse_input(INPUT)));
     }
 
     #[test]
@@ -166,6 +220,9 @@ mod tests {
             Point::new(1, 1),
             Point::new(2, 1),
             Point::new(3, 1),
+            Point::new(0, 0),
+            Point::new(1, 0),
+            Point::new(2, 0),
         ]);
 
         assert_eq!(part_number.neighbors(), expected);
