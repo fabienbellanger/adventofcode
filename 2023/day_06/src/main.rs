552c0b963d559c3a8b1d5ac84bd3a6018b49ebf0
Fabien <fabienbellanger@users.noreply.github.com>
2023-12-06 23:22:37 +0100

    WIP

--- 2023/day_06/src/main.rs
+++ 2023/day_06/src/main.rs
@@ -1,11 +1,11 @@
 #![allow(unused_variables)]
-use utils::data::file_to_vec_string;
+use std::fs;
 
 const INPUT: &str = "input.txt";
 
 fn main() {
-    println!("Part 1 result: {}", part1(file_to_vec_string(INPUT)));
-    println!("Part 2 result: {}", part2(file_to_vec_string(INPUT)));
+    println!("Part 1 result: {}", part1(parse_input(INPUT)));
+    println!("Part 2 result: {}", part2(parse_input(INPUT)));
 }
 
 fn part1(data: Vec<String>) -> usize {
@@ -16,6 +16,18 @@ fn part2(data: Vec<String>) -> usize {
     todo!()
 }
 
+fn parse_input(file: &str) -> Vec<String> {
+    let lines = fs::read_to_string(file)
+        .expect("Cannot read the file input.txt")
+        .trim()
+        .lines()
+        .map(|l| l.to_string())
+        .collect::<Vec<_>>();
+    dbg!(lines);
+
+    vec![]
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,13 +36,13 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(0, part1(file_to_vec_string(TEST)));
-        // assert_eq!(0, part1(file_to_vec_string(INPUT)));
+        assert_eq!(288, part1(parse_input(TEST)));
+        // assert_eq!(0, part1(parse_input(INPUT)));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(0, part2(file_to_vec_string(TEST)));
-        // assert_eq!(0, part2(file_to_vec_string(INPUT)));
+        // assert_eq!(0, part2(parse_input(TEST)));
+        // assert_eq!(0, part2(parse_input(INPUT)));
     }
 }
