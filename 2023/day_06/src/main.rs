29362aab0ebe0a6cfa581dd7740cc120b723cd85
Fabien Bellanger <fabien.bellanger@apitic.com>
2023-12-06 17:40:08 +0100

    Init Day 6

---
+++ 2023/day_06/src/main.rs
@@ -0,0 +1,36 @@
+#![allow(unused_variables)]
+use utils::data::file_to_vec_string;
+
+const INPUT: &str = "input.txt";
+
+fn main() {
+    println!("Part 1 result: {}", part1(file_to_vec_string(INPUT)));
+    println!("Part 2 result: {}", part2(file_to_vec_string(INPUT)));
+}
+
+fn part1(data: Vec<String>) -> usize {
+    todo!()
+}
+
+fn part2(data: Vec<String>) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST: &str = "test.txt";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(0, part1(file_to_vec_string(TEST)));
+        // assert_eq!(0, part1(file_to_vec_string(INPUT)));
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(0, part2(file_to_vec_string(TEST)));
+        // assert_eq!(0, part2(file_to_vec_string(INPUT)));
+    }
+}
